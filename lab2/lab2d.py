6a9fe3dff5095782b4dea034fd759980d3f40f17
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-01-21 16:55:34 -0500

    Added lab2d

---
+++ lab2/lab2d.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Antonio Karlo Mijares
+
+# Imports sys
+import sys
+
+#Check if file called has 2 arguments
+#If it does, it prints out the info
+if len(sys.argv) == 3:
+	name = sys.argv[1]
+	age = sys.argv[2]
+	print ('Hi ' + name + ', you are ' + age + ' years old.')
+	sys.exit()
+	
+
+#If file has no or too many arguments,
+#It prints out this message
+print ('Usage: ./lab2d.py name age')
+sys.exit()
