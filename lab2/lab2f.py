3c15dc29d90af9f1f5188cb308653a7ae5edeb47
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-01-22 08:41:17 -0500

    Added lab2f

---
+++ lab2/lab2f.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Antonio Karlo Mijares
+
+# Uses a while loop to print out the
+# subtracted number. When it reaches
+# 0, the while loop breaks and prints
+# out a message.
+
+# Import sys
+import sys
+
+timer = int(sys.argv[1])
+while True:
+    print(timer)
+    timer = timer - 1
+    if timer == 0:
+        print('blast off!')
+        break
