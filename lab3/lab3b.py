6cef8e162b44627922c6cd05861b892193f0a745
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-01-25 13:22:03 -0500

    Added lab3b

--- lab3/lab3b.py
+++ lab3/lab3b.py
@@ -2,3 +2,20 @@
 
 #Antonio Karlo Mijares
 
+
+def sum_numbers(number1, number2):
+    addednum = int(number1) + int(number2)
+    return addednum
+
+def subtract_numbers(number1, number2):
+    subtractednum = int(number1) - int(number2)
+    return subtractednum
+
+def multiply_numbers(number1, number2):
+    multipliednum = int(number1) * int(number2)
+    return multipliednum
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
