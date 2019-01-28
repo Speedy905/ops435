cd098f75cf748d2e77bdaa391e17f03cf181c880
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-01-28 14:49:07 -0500

    Updated lab3d
    Added the function
    but it still does not work properly

--- lab3/lab3d.py
+++ lab3/lab3d.py
@@ -2,3 +2,13 @@
 
 #Antonio Karlo Mijares
 
+#Import os and sys
+import os
+
+def free_space():
+	remaining = os.system("df -h | grep '/$' | awk '{print $4}'")
+	utf = remaining[0].decode('utf-8').strip()
+	print(remaining)
+	
+
+free_space()
