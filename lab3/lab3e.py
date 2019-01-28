e6d352040085245c6e7567e82121998b0bb3c9a6
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-01-28 17:34:11 -0500

    Updated lab3e
    Updated functions

--- lab3/lab3e.py
+++ lab3/lab3e.py
@@ -2,29 +2,34 @@
 
 #Antonio Karlo Mijares
 
-# Create the list called "my_list" here, not within any function defined below.
+# Create the list called "my_list" here, not within any function defined 
+# below.
 # That makes it a global variable. We'll talk about that in another lab.
 
-my_list= [ ]
+my_list= [ 100, 200, 300, 'six hundred']
 
 def give_list():
     # Does not accept any arguments
     # Returns all of the global variable my_list unchanged
+    return my_list
 
 def give_first_item():
     # Does not accept any arguments
     # Returns a single string that is the 
     #first item in the global my_list
+    return my_list[0]
 
 def give_first_and_last_item():
     # Does not accept any arguments
     # Returns a list that includes the first and last items 
     #in the global my_list
+    return my_list[0], my_list[-1]
 
 def give_second_and_third_item():
     # Does not accept any arguments
     # Returns a list that includes the second and third items in 
     # the global my_list
+    return my_list[1], my_list[2]
 
 if __name__ == '__main__':   
 	# This section also referred to as a "main code"
