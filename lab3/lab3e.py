6cddc851a4e27921431d1b52b621bb5596e16d31
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-01-28 17:24:44 -0500

    Added lab3e
    File won't work yet

---
+++ lab3/lab3e.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+#Antonio Karlo Mijares
+
+# Create the list called "my_list" here, not within any function defined below.
+# That makes it a global variable. We'll talk about that in another lab.
+
+my_list= [ ]
+
+def give_list():
+    # Does not accept any arguments
+    # Returns all of the global variable my_list unchanged
+
+def give_first_item():
+    # Does not accept any arguments
+    # Returns a single string that is the 
+    #first item in the global my_list
+
+def give_first_and_last_item():
+    # Does not accept any arguments
+    # Returns a list that includes the first and last items 
+    #in the global my_list
+
+def give_second_and_third_item():
+    # Does not accept any arguments
+    # Returns a list that includes the second and third items in 
+    # the global my_list
+
+if __name__ == '__main__':   
+	# This section also referred to as a "main code"
+    print(give_list())
+    print(give_first_item())
+    print(give_first_and_last_item())
+    print(give_second_and_third_item())
