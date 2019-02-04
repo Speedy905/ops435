56cc48df2c074ded5e7a6f2b06bfc35526970e84
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-02-04 10:59:15 -0500

    Updated lab4a

--- lab4/lab4a.py
+++ lab4/lab4a.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env 
+#!/usr/bin/env python3 
 
 # Antonio Karlo Mijares
 
 def join_sets(s1, s2):
     # join_sets will return a set that contains 
     # every value from both s1 and s2
+    return s1 | s2
 
 def match_sets(s1, s2):
     # match_sets will return a set that contains 
     # all values found in both s1 and s2
+    return s1 & s2
 
 def diff_sets(s1, s2):
     # diff_sets will return a set that contains 
     # all different values which are not shared between the sets
+    return s1 ^ s2
 
 if __name__ == '__main__':
     set1 = set(range(1,10))
