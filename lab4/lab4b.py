c0063a54cc081c913f23d373214cc3429945b402
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-02-05 08:35:14 -0500

    Fixed functions

--- lab4/lab4b.py
+++ lab4/lab4b.py
@@ -5,17 +5,19 @@
 def join_lists(l1, l2):
     # join_lists will return a list that contains 
     # every value from both l1 and l2
-    return l1 + l2
+    return list(set(l1) | set(l2))
     
 def match_lists(l1, l2):
     # match_lists will return a list that contains 
     # all values found in both l1 and l2
-    return set(l1) & set(l2)
+    matchingset = set(l1).intersection(set(l2))
+    matchinglist = list(matchingset)
+    return matchinglist
 
 def diff_lists(l1, l2):
     # diff_lists will return a list that contains 
     # all different values, which are not shared between the lists
-    return set(l1) - set(l2)
+    return list(set(l1) ^ set(l2))
 
 if __name__ == '__main__':
     list1 = list(range(1,10))
