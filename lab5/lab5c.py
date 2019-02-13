d2673a21f94abf99b8785c161f8cd0aa5675474f
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-02-12 20:52:01 -0500

    Updated lab5c

--- lab5/lab5c.py
+++ lab5/lab5c.py
@@ -2,3 +2,30 @@
 
 #Antonio Karlo Mijares
 
+def add(number1, number2):
+    # Add two numbers together, return the result, if error return string 'error: could not add numbers'
+    try:
+        t = int(number1) + number2
+        return t
+    except:
+        sumerror = 'error: could not add numbers'
+        return sumerror
+    
+def read_file(filename):
+    # Read a file, return a list of all lines, if error return string 'error: could not read file'
+    try:
+        f=open(filename, 'r')
+        t = f.read()
+        s = t.splitlines()
+        f.close()
+        return (s)
+    except:
+        fileerror = 'error: could not read file'
+        return fileerror
+
+if __name__ == '__main__':
+    print(add(10,5))                        # works
+    print(add('10',5))                      # works
+    print(add('abc',5))                     # exception
+    print(read_file('seneca2.txt'))         # works
+    print(read_file('file10000.txt'))       # exception
