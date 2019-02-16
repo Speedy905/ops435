9707d5b7ba5497b832894ada4bcd6496a30743cf
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-02-16 13:28:05 -0500

    Created akmijares.py

---
+++ A1/akmijares.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 3
+
+#OPS435 Assignment 1 - Winter 2019
+#Program: akmijares.py (replace student_id with your Seneca User name)
+#Author: "Antonio Karlo Mijares"
+#The python code in this file (akmijares.py) is original work written by
+#Antonio Karlo Mijares. No code in this file is copied from any other source 
+#except those provided by the course instructor, including any person, 
+#textbook, or on-line resource. I have not shared this python script 
+#with anyone or anything except for submission for grading.  
+#I understand that the Academic Honesty Policy will be enforced and 
+#violators will be reported and appropriate action will be taken.
