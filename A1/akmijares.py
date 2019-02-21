7e35ff50518209833f6fa1b84c8052108ad105e7
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-02-20 21:45:29 -0500

    Renamed variables

--- A1/akmijares.py
+++ A1/akmijares.py
@@ -29,13 +29,13 @@ def usage():
 # it calls the usage function
 # Otherwise it goes to a loop where it calls
 # Either the tomorrow, or the yesterday function
-def dbda(date, days):
+def dbda(date, day):
     error= usage()
     try:
-        x = int(days)
+        x = int(day)
         tmp_date = date
-        vdate = valid_date(date)
-        if vdate == False:
+        va_date = valid_date(date)
+        if va_date == False:
             sys.exit()
         else:
             while x != 0:   
@@ -46,8 +46,8 @@ def dbda(date, days):
                     tmp_date = yesterday(tmp_date)
                     x = x + 1
         error= usage()
-        target_day = tmp_date 
-        return target_day
+        target = tmp_date 
+        return target
     except ValueError:
         return error
 
@@ -79,6 +79,7 @@ def tomorrow(today):
             year = year +  1
         else:
             to_month = tmp_month + 0
+
         next_date = str(year)+str(to_month).zfill(2)+str(to_day).zfill(2)
      
         return next_date    
@@ -87,34 +88,35 @@ def tomorrow(today):
 # Subtracts the number of days before date
 # If subtracting goes to an older month/year
 # It adapts to the new month/year
-def yesterday(today):
-    if len(today) != 8:
+def yesterday(current):
+    if len(current) != 8:
        print(usage())
     else:
-        year = int(today[0:4])
-        month = int(today[4:6])
-        day = int(today[6:])
-        tmp_day = day - 1 
+        currentyear = int(current[0:4])
+        currentmonth = int(current[4:6])
+        currentday = int(current[6:])
+        currentday_tmp = currentday - 1 
     
-        month_max = days_in_mon(year)
+        month_max = days_in_mon(currentyear)
        
-        tmp_month = month
-        tmp_day = day - 1 
-        if tmp_day < month_max[month]:
-            to_day = tmp_day % month_max[month] 
+        tmp_month = currentmonth
+        currentday_tmp = currentday - 1 
+
+        if currentday_tmp < month_max[currentmonth]:
+            to_day = currentday_tmp % month_max[currentmonth] 
             tmp_month = tmp_month - 1
             
         else:
-           to_day = tmp_day
-           tmp_month = month - 0
+           to_day = currentday_tmp
+           tmp_month = currentmonth - 0
         
         if tmp_month < 1:
             to_month = 1
-            year = year -  1
+            currentyear = currentyear -  1
         else:
             to_month = tmp_month - 0
 
-        before_date = str(year)+str(to_month).zfill(2)+str(to_day).zfill(2)
+        before_date = str(currentyear)+str(to_month).zfill(2)+str(to_day).zfill(2)
 
         return before_date
 
@@ -122,32 +124,33 @@ def yesterday(today):
 # While it also checks if it is a leap year
 # It also stores the max day in a tuple
 def days_in_mon(tmp_year):
-    lpyear = tmp_year
-    if lpyear % 4 ==  0:
-        feb_max = 29 
-    elif lpyear % 100 == 0:
-        feb_max = 28 
-    elif lpyear % 400 == 0:
-        feb_max = 29
+    leapyear = tmp_year
+    if leapyear % 4 ==  0:
+        febmax = 29 
+    elif leapyear % 100 == 0:
+        febmax = 28 
+    elif leapyear % 400 == 0:
+        febmax = 29
     else:
-        feb_max = 28 
-    leap_year(lpyear)
-    mon_max = {1:31, 2:feb_max, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 
+        febmax = 28 
+
+    leap_year(leapyear)
+    month_max = {1:31, 2:febmax, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 
     9:30, 10:31, 11:30, 12:31}
-    return mon_max
+
+    return month_max
     
 # leap_year function
 # Check if year is leapyear
 # If it is, returns as True
 # If not, returned as False
-def leap_year(lpyear):
-    
-    leap_year = lpyear
-    if leap_year % 4 ==  0:
+def leap_year(leap):
+    l_year = leap
+    if l_year % 4 ==  0:
         return True
-    elif leap_year % 100 == 0:
+    elif l_year % 100 == 0:
         return False
-    elif leap_year % 400 == 0:
+    elif l_year % 400 == 0:
         return True
     else:
         return False
@@ -158,19 +161,18 @@ def leap_year(lpyear):
 # Statements to see if valid
 # If valid, returns as True
 # If not, prints an error.
-def valid_date(today):
-    
-    year = int(today[0:4])
-    month = int(today[4:6])
-    day = int(today[6:])
-    tmp_day = day
-    tmp_month = month
-    days_month = days_in_mon(year)
-    if tmp_month > 12 or tmp_month < 1:
+def valid_date(date):
+    y = int(date[0:4])
+    m = int(date[4:6])
+    d = int(date[6:])
+    tmp_d = d
+    tmp_m = m
+    days_in_month = days_in_mon(y)
+    if tmp_m > 12 or tmp_m < 1:
         print(showmonth())
         return False
     else:
-        if tmp_day > days_month[month]:
+        if tmp_d > days_in_month[m]:
             print(showday())
             return False
         else:
