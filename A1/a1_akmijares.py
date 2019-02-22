2e1d7030364a9f4d274c385fba1428a8578a4f25
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-02-22 11:17:43 -0500

    Revised comment block

--- A1/a1_akmijares.py
+++ A1/a1_akmijares.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
 # OPS435 Assignment 1 - Winter 2019
-# Program: akmijares.py (replace student_id with your Seneca User name)
+# Program: akmijares.py
 # Author: "Antonio Karlo Mijares"
 # The python code in this file (akmijares.py) is original work written
-# by
-# Antonio Karlo Mijares. No code in this file is copied from any other
-# source
-# except those provided by the course instructor, including any person,
-# textbook, or on-line resource. I have not shared this python script
-# with anyone or anything except for submission for grading.
-# I understand that the Academic Honesty Policy will be enforced and
-# violators will be reported and appropriate action will be taken.
+# by #Antonio Karlo Mijares. No code in this file is copied from any 
+# other source except those provided by the course instructor, 
+# including any person, textbook, or on-line resource. 
+# I have not shared this python script# with anyone or anything 
+# except for submission for grading. I understand that the 
+# Academic Honesty Policy will be enforced and violators will be 
+# reported and appropriate action will be taken.
 
 # Imports the sys module
 import sys
