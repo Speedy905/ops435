4ac50aeda6dad72194e4c5d9b8295322cd605995
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-03-19 14:38:31 -0400

    Changed code

--- lab7/lab7b.py
+++ lab7/lab7b.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 def function1():
-    # This variable 'a' is completely unrelated to the variable 'a' 
-    #in function2():
+    # This variable 'a' is completely unrelated to the variable 
+    #'a' in function2():
     a = 'Andrew 1'
     print(a)
 
 def function2():
-    # This variable 'a' is completely unrelated to the variable 'a' in function1():
+    # This variable 'a' is completely unrelated to the variable 
+    #'a' in function1():
     a = 'Andrew 2'
     print(a)
 
-# Note that you cannot make any of the following print() functions work because neither of 
-# the variables '''a''' exist in this scope 
-#(outside the function where they were defined):
+# Note that you cannot make any of the following print() functions work 
+#because neither of 
+# the variables '''a''' exist in this scope (outside the function 
+#where they were defined):
 a = 'Andrew 500'
 print(a)
 function1()
