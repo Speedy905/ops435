d40da552f0fb1ef752778e06b954a45d3b278532
Antonio Karlo Mijares <AK.Mijares99@gmail.com>
2019-03-28 12:51:10 -0400

    Added stuff

--- lab8/fabfile.py
+++ lab8/fabfile.py
@@ -4,7 +4,35 @@
 
 from fabric.api import *
 
+env.user = 'root'
+
 # Will get the hostname of this worker:
 def getHostname():
     name = run("hostname")
     print(name)
+
+# Will set up a working web server with a pre-built website
+def setupWebServer():
+    run("hostnamectl set-hostname www")
+    run("yum -y -d1 install httpd")
+    run("systemctl enable httpd")
+    run("systemctl start httpd")
+    #with cd("/var/www/html"):
+     #   put("webcontents.tar.bz2", ".")
+      #  run("tar xvf webcontents.tar.bz2")
+       # run("rm webcontents.tar.bz2")
+    #run("./extract.bash")
+
+# Will uninstall firewalld and replace it with iptables
+def setupFirewall():
+	run("systemctl disable firewalld")
+	run("systemctl stop firewalld")
+	run("yum -y -d1 remove firewalld")
+	run("yum -y -d1 install iptables-services")
+	run("systemctl enable iptables")
+	run("systemctl start iptables")
+	with settings(warn_only=True):
+		firewallAlreadySetUp = run("iptables -C INPUT -p tcp --dport 80 -j ACCEPT")
+        if firewallAlreadySetUp.return_code == 1:
+        	run("iptables -I INPUT -p tcp --dport 80 -j ACCEPT")
+    		run("iptables-save > /etc/sysconfig/iptables")
